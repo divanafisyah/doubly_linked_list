540050255263ffd65d3a1e46816d4b7b33d9fa98
Diva Nafisyah <diva.nafisyah.ft21@mail.umy.ac.id>
2022-11-14 20:03:57 +0700

    add class DoubleLinkedList

--- double_linked_list/Program.cs
+++ double_linked_list/Program.cs
@@ -38,11 +38,49 @@ namespace double_linked_list
             Node newNode = new Node();
             newNode.noMhs = nim;
             newNode.name = nm;
-        }
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
+
+            //check if the list empty
+            if (START == null || nim <= START.noMhs)
+            {
+                if ((START != null) && (nim == START.noMhs))
+                {
+                    Console.WriteLine("\nDuplicate number not allowed");
+                    return;
+                }
+                newNode.next = START;
+                if (START != null)
+                    START.prev = newNode;
+                newNode.prev = null;
+                START = newNode;
+                return;
+            }
+            /*if the node is to be inserted at between two node*/
+            Node previous, current;
+            for (current = previous = START;
+                current != null && nim >= current.noMhs;
+                previous = current, current = current.next)
+            {
+                if (nim == current.noMhs)
+                {
+                    Console.WriteLine("\nDuplicate roll numbers not allowed");
+                    return;
+                }
+            }
+            /*On the execution of the above for loop, prev and
+             * * current will point to those nodes
+             * * between wich the new node is to be inserted*/
+            newNode.next = current;
+            newNode.prev = previous;
+
+            //if the node is to be insarted at the end of the list
+            if (current == null)
+            {
+                newNode.next = null;
+                previous.next = newNode;
+                return;
+            }
+            current.prev = newNode;
+            previous.next = newNode;
         }
     }
 }
