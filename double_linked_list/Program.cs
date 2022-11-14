833ef3683e8b9e88a557e567018c35961744e687
Diva Nafisyah <diva.nafisyah.ft21@mail.umy.ac.id>
2022-11-14 16:54:47 +0700

    Add project files.

---
+++ double_linked_list/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace double_linked_list
+{
+    class Node
+    {
+        /* Node class represents the node of doubly linked list
+         * it consist of the information part and links to
+         * in terms of next and previous*/
+        public int noMhs;
+        public string name;
+        //point to the succeding node
+        public Node next;
+        //point to the precceeding node
+        public Node prev;
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
